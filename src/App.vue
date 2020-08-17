b7972cbe55ce3aa0571471a8e1c394f6ea378142
Bhavartha Khawale <bhavarthakhawale@gmail.com>
2020-08-17 16:52:30 +0530

    Changes logo and added input field

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,44 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div id="app" class="container">
+    <img src="https://image.flaticon.com/icons/svg/748/748646.svg" class="logo" />
+    <TodoList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from "./components/HelloWorld";
+import TodoList from "./components/TodoList";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    TodoList,
+  },
+};
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+.logo {
+  margin: 20px auto;
+  height: 75px;
+  display: block;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/TodoList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo"/>
+    Todo List goes here
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  data() {
+    return {
+      msg: "Welcome to Your TodoList App",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+
+.todo-input{
+  width: 100%;
+  padding: 10px 18px;
+  margin-bottom: 20px;
+  font-size: 18px;
+  &:focus{
+    outline: 0;
+  }
+}
+
+</style>  
